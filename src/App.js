38054ea8f2b2db2b674071f79ac3948ea6f4b5dc
vivek133kr <127092887+vivek133kr@users.noreply.github.com>
2023-05-10 12:25:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from "react";
+function App() {
+  const [mobileOperator, setMobileOperator] = useState("");
+
+  useEffect(() => {
+   
+    const fetchMobileInfo = async () => {
+      try {
+        const response = await fetch(
+          "https://api.ipdata.co?api-key=d45e27bf8e1bf87c780a0e478208df948ae60edd8a209022baca0e95"
+        );
+        const data = await response.json();
+        console.log(data, " line 14")
+        setMobileOperator(data.mobile.carrier || "Unknown");
+      } catch (error) {
+        console.error("Error fetching mobile information:", error);
+      }
+    };
+
+    fetchMobileInfo();
+  }, []);
+
+  return (
+    <div>
+      <p>Mobile Operator: {mobileOperator}</p>
+    </div>
+  );
+}
+
+export default App;
